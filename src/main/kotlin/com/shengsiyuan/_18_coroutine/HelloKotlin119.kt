6e409a0e40869664a4677292c0742d4a5241ff4e
hemulin <mulin.he@gotechcn.cn>
2021-01-11 22:50:36 +0800

    lesson 56

--- src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin119.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin119.kt
@@ -1,2 +1,20 @@
 package com.shengsiyuan._18_coroutine
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    GlobalScope.launch {
+        delay(1000)
+        println("Kotlin Coroutine")
+    }
+    println("hello")
+    //runBlocking 阻塞线程
+    val end = runBlocking {
+        delay(2000)
+        return@runBlocking "ABC"
+    }
+    println("world $end")
+}
--- src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin120.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin120.kt
@@ -1,2 +1,16 @@
 package com.shengsiyuan._18_coroutine
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    GlobalScope.launch {
+        delay(1000)
+        println("Kotlin Coroutine")
+    }
+    println("Hello ")
+    delay(2000)
+    println("world")
+}
