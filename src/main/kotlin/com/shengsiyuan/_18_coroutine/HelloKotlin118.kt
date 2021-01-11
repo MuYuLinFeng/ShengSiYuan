250a37400b600bab18ce870b1c15a8f5a550554a
hemulin <mulin.he@gotechcn.cn>
2021-01-11 22:21:47 +0800

    lesson 55

--- src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin118.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin118.kt
@@ -3,11 +3,10 @@ package com.shengsiyuan._18_coroutine
 import kotlin.concurrent.thread
 
 fun main() {
-    val thread = thread {
+   thread {
         Thread.sleep(1000)
         println("Kotlin Thread")
     }
-    thread.start()
     println("hello")
     Thread.sleep(2000)
     println("world")
