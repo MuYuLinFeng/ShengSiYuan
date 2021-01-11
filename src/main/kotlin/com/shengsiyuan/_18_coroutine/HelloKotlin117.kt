c740477048e5b204829ecc99f64b8a68789d7266
hemulin <mulin.he@gotechcn.cn>
2021-01-11 22:16:44 +0800

    lesson 55

--- src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin117.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin117.kt
@@ -5,11 +5,16 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun main() {
+    //launch函数不会阻塞线程执行，而是在后台新创建一个协程。
     GlobalScope.launch {
         delay(1000)
         println("Kotlin Coroutines")
     }
     println("hello")
+    /*
+        如果这里睡眠500毫秒的话，协程块儿里面的打印将不会执行。
+        因为线程在500毫秒之后就执行并结束了，这时候协程还没有来得及执行。
+     */
     Thread.sleep(2000)
     println("world")
 }
---
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin119.kt
@@ -0,0 +1,2 @@
+package com.shengsiyuan._18_coroutine
+
