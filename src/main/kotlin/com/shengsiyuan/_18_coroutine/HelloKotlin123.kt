136709ca1086bc88a5095e2d92586bf7ae804fbf
hemulin <mulin.he@gotechcn.cn>
2021-01-12 18:28:31 +0800

    lesson 60

--- src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin123.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/HelloKotlin123.kt
@@ -10,8 +10,13 @@ import kotlinx.coroutines.runBlocking
     该构建器会创建一个协程作用域，并且会等待所有启动的子协程全部完成后自身才会完成。
 
     runBlocking与coroutineScope之间的主要差别在于，
-    后者在等待所有子协程完成其任务是并不会阻塞当前的线程。
+    后者(coroutineScope builder)在等待所有子协程完成其任务是并不会阻塞当前的线程。
     而runBlocking会阻塞主线程。
+
+    runBlocking 并非挂起函数；也就是说，调用它的线程会一直位于该函数之中，直到携程执行完毕为止。
+    coroutineScope 是挂起函数；也就是说如果其中的携程挂起，那么coroutineScope函数也会挂起，
+        这样创建coroutineScope的外层函数就可以继续在同一个线程中执行了。
+        该线程会「逃离」coroutineScope之外，并且可以做其他一些事情。
  */
 fun main() = runBlocking {
     launch {
@@ -19,9 +24,21 @@ fun main() = runBlocking {
         println("my job1")
     }
     println("person")
-    //老师：coroutineScope 等待所有子协程结束后自身才会结束
-    //弹幕：welcome最后输出是因为 coroutineScope 方法是 suspend的
-    //和delay一样都是会阻塞流程继续，但是不会阻塞线程。
+    /*
+        老师：coroutineScope 等待所有子协程结束后自身才会结束
+     */
+
+    /*
+        弹幕：welcome最后输出是因为 coroutineScope 方法是 suspend的
+            和delay一样都是会阻塞流程继续，但是不会阻塞线程。
+     */
+
+    /*
+        我：我们客观看到的是代码的流程是否继续，在挂起函数 coroutineScope执行期间，
+            线程跑去执行coroutineScope之前的事件了
+            等待coroutineScope执行结束回来。
+            而不是像runBlocking客观的阻塞了线程使得代码流程停止。
+     */
     coroutineScope {
         launch {
             delay(20000)
