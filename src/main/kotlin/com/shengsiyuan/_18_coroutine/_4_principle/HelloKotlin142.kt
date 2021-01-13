01b40368cb3273d0373c2a9faeb11ebfdf67665f
hemulin <mulin.he@gotechcn.cn>
2021-01-13 23:08:01 +0800

    lesson 69

--- src/main/kotlin/com/shengsiyuan/_18_coroutine/_4_principle/HelloKotlin142.kt
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/_4_principle/HelloKotlin142.kt
@@ -28,22 +28,39 @@ import java.util.concurrent.Executors
     对于该示例来说，它会继承runBlocking的上下文，而runBlocking则是运行在main线程当中。
     2.Dispatchers.Unconfined是一种很特殊的协程分发器，它在该示例中也是运行在main线程中，
     但实际上，其运行机制与不指定协程分发器时是完全不同的。
+    3.Dispatchers.Default是默认的分发器，当协程是通过GlobalScope来启动的时候，
+    它会使用改默认的分发器来启动协程，它会使用一个后台的共享线程池来运行我们的协程代码。
+    因此，launch(Dispatchers.Default)等价于GlobalScope.launch{}
+    4.ThreadExecutors创建一个线程池，改线程池中的线程用来执行我们所制定的协程代码；
+    在实际开发中，使用专门的线程来执行协程代码的代价是非常高的，因此在协程代码执行完毕后，
+    我们必须要释放相应的资源，这里就需要使用close方法来关闭相应的协程分发器，从而释放掉资源；
+    也可以将该协程分发器存储到一个顶层变量中，以便在程序的其它地方进行复用。
  */
 
 fun main() = runBlocking<Unit> {
     launch {
         println("no params   ,thread: ${Thread.currentThread().name}")
     }
-    //不理解为啥launch声明的参数没有Dispatchers，但是传了个Dispatchers做参数
+
+    //launch声明的参数没有Dispatchers，但是传了个Dispatchers做参数
+    //CoroutineDispatchers间接实现了CoroutineContext
     launch(Dispatchers.Unconfined) {
         println("Dispatchers.Unconfined   ,thread: ${Thread.currentThread().name}")
         //delay方法加或者不加 加在前面或者后面都会影响执行的线程
         delay(100)
     }
+
     launch(Dispatchers.Default) {
         println("Dispatchers.Default   ,thread: ${Thread.currentThread().name}")
     }
-    launch(Executors.newSingleThreadExecutor().asCoroutineDispatcher()) {
+
+    val thread = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+    launch(thread) {
         println("Single Thread Executors   ,thread: ${Thread.currentThread().name}")
+        thread.close()
+    }
+
+    GlobalScope.launch {
+        println("GlobalScope.launch   ,thread: ${Thread.currentThread().name}")
     }
 }
