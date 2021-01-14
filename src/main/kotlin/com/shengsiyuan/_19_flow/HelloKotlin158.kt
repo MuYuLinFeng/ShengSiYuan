69d20e39db3f7c5f32356ef85629fcb2984c9ef8
hemulin <mulin.he@gotechcn.cn>
2021-01-14 23:59:41 +0800

    lesson 79 流构建器

--- src/main/kotlin/com/shengsiyuan/_19_flow/HelloKotlin158.kt
+++ src/main/kotlin/com/shengsiyuan/_19_flow/HelloKotlin158.kt
@@ -1,1 +1,35 @@
 package com.shengsiyuan._19_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+/*
+    Flow的取消
+    Flow的取消实际上与协程的取消之间是一种协同的关系；
+    对于Flow来说，它自身并没有引入任何新的取消点这样的概念；它对于取消时完全透明的。
+
+    Flow的收集操作是可以取消的，前提是Flow在一个可取消的挂起函数中（如yield）中被挂起了，
+    除此之外，我们无法通过其他方式来取消Flow的执行。
+ */
+
+private fun myMethod(): Flow<Int> = flow {
+    for (i in 1..5) {
+        delay(100)
+        println("Emit $i")
+        emit(i)
+    }
+}
+
+fun main() = runBlocking {
+    val job = withTimeoutOrNull(320) {
+        myMethod().collect {
+            println(it)
+        }
+    }
+    println(job)
+    println("Finished")
+}
--- src/main/kotlin/com/shengsiyuan/_19_flow/HelloKotlin159.kt
+++ src/main/kotlin/com/shengsiyuan/_19_flow/HelloKotlin159.kt
@@ -1,1 +1,34 @@
 package com.shengsiyuan._19_flow
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+/*
+    Flow builder(流构建器)
+
+    1.flow是最经常被使用的一种流构建器
+    2.flowOf(...) 构建器可用于定义能够发射固定数量值的流
+    3.对于各种集合与序列来说，他们都提供了asFlow扩展方法来将自身转换成Flow
+
+    channelFlow { ... }
+    MutableStateFlow
+    MutableSharedFlow
+
+    inline，noinline，crossinline
+    non-local returns 非局部返回 函数内部的函数直接返回一个值作为外部函数的返回值
+    非局部返回的前提要求外部方法是一个inline的
+
+    crossinline 标识的方法不允许非局部返回
+
+ */
+
+fun main() = runBlocking {
+    (1..5).asFlow().collect {
+        println("asFlow $it")
+    }
+    flowOf(500, 20, 321, 54).collect {
+        println("flowOf $it")
+    }
+}
