a57b107d453c2bde71063af8e5bffe8edc3160ff
hemulin <mulin.he@gotechcn.cn>
2021-01-14 16:08:51 +0800

    lesson 72 协程中的Job

---
+++ src/main/kotlin/com/shengsiyuan/_18_coroutine/_6_job/HelloKotlin146.kt
@@ -0,0 +1,27 @@
+package com.shengsiyuan._18_coroutine._6_job
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.job
+import kotlinx.coroutines.runBlocking
+
+/*
+    Job的使用方式以及在Context中的具体应用
+
+    协程的Job是归属于其上下文（CoroutineContext）的一部分，
+    Kotlin为我们提供了一种简洁的手段来通过协程上下文获取到协程自身的Job对象
+
+    我们可以通过coroutineContext[Job]表达式来访问上下文中的Job对象
+ */
+
+fun main() = runBlocking {
+    //coroutineContext代表当前协程的上下文
+    val job: Job? = coroutineContext[Job]
+    println(job)//BlockingCoroutine{Active}@4ccabbaa
+
+    val job2 = coroutineContext.job
+    println(job2)
+
+    println(coroutineContext.isActive)
+    println(job?.isActive)
+}
